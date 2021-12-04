d1b9f2f8e25c2c094fc7e25ad24c033053f977a2
Regina Battle <hello@reginabattle.com>
2021-12-04 00:30:26 -0500

    more clean up

--- gatsby-browser.js
+++
@@ -1,5 +0,0 @@
-// normalize CSS across browsers
-import "./src/css/normalize.css"
-
-// custom CSS styles
-import "./src/css/style.css"
--- gatsby-config.js
+++ gatsby-config.js
@@ -3,9 +3,7 @@ module.exports = {
     {
       resolve: `gatsby-source-wordpress`,
       options: {
-        url:
-          process.env.WPGRAPHQL_URL ||
-          `https://wpgatsbydemo.wpengine.com/graphql`,
+        url: process.env.WPGRAPHQL_URL,
       },
     },
     {
