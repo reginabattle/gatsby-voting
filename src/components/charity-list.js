d28d326dbaf732f87b2c2a670b6e8cb5140e23ea
Regina Battle <hello@reginabattle.com>
2021-12-05 22:10:11 -0500

    add button

--- src/components/charity-list.js
+++ src/components/charity-list.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react"
 import Card from "./card"
+import Button from "./button"
 
 const CharityList = ({ charities }) => {
   const [vote, setVote] = useState()
@@ -9,20 +10,32 @@ const CharityList = ({ charities }) => {
     setVote(title)
   }
 
+  const handleSubmit = () => {
+    console.log("submitted")
+  }
+
   return (
-    <div className="charity-list">
-      {charities.map(item => {
-        const { charity } = item
-        return (
-          <Card
-            key={item.id}
-            title={item.title}
-            data={charity.details}
-            callback={handleClick}
-          />
-        )
-      })}
-    </div>
+    <>
+      <div className="charity-list">
+        {charities.map(item => {
+          const { charity } = item
+          return (
+            <Card
+              key={item.id}
+              title={item.title}
+              data={charity.details}
+              callback={handleClick}
+            />
+          )
+        })}
+      </div>
+
+      <Button
+        className="charity-list__button"
+        label="Support"
+        onClick={handleSubmit}
+      />
+    </>
   )
 }
 
