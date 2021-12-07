0dffb8599f47c8abd7a72fb0c319e10ce959bf09
Regina Battle <hello@reginabattle.com>
2021-12-07 15:39:03 -0500

    update env variable name

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
 export async function updateCount(id, count) {
-  fetch(`${process.env.WP_API_URL}/charities/${id}`, {
+  fetch(`${process.env.GATSBY_API_URL}/charities/${id}`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -9,6 +9,24 @@ export async function updateCount(id, count) {
     body: JSON.stringify({
       acf: {
         count: count + 1,
+      },
+    }),
+  }).then(res => {
+    console.log("res", res)
+  })
+}
+
+export async function updateVisits(addresses, ip) {
+  fetch(`${process.env.GATSBY_API_URL}/pages/2`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      Authorization: "Bearer " + process.env.JWT_TOKEN,
+    },
+    body: JSON.stringify({
+      acf: {
+        ip_addresses: `${addresses}, ${ip}`,
       },
     }),
   }).then(res => {
