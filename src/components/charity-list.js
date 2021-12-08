0c5c7e920881ac565dfce8bb14efaec98b340d91
Regina Battle <hello@reginabattle.com>
2021-12-07 23:28:43 -0500

    get ip address

--- src/components/charity-list.js
+++ src/components/charity-list.js
@@ -2,14 +2,22 @@ import React, { useState } from "react"
 import Card from "./card"
 import Button from "./button"
 import { updateCount, updateVisits } from "../utils/api"
+import publicIp from "public-ip"
 
 const CharityList = ({ charities, visits }) => {
   const [vote, setVote] = useState("")
   const [count, setCount] = useState(0)
+  const [currentIp, setCurrentIp] = useState("")
+
+  ;(async () => {
+    const ip = await publicIp.v4()
+    setCurrentIp(ip)
+  })()
 
   const handleClick = (id, votes) => {
     setVote(id)
     setCount(votes)
+    updateVisits(visits.ipAddresses, currentIp)
   }
 
   const handleSubmit = (e, id) => {
