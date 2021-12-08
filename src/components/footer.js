757e778f78934c6330c72a85d6f17ae5e04c8253
Regina Battle <hello@reginabattle.com>
2021-12-07 22:46:38 -0500

    footer logo

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,27 +1,16 @@
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
 
 const Footer = () => {
-  const data = useStaticQuery(graphql`
-    query logoQuery {
-      image: wpMediaItem(slug: { eq: "sti-logo" }) {
-        sourceUrl
-        altText
-      }
-    }
-  `)
-
-  const { image } = data
   return (
     <footer className="footer">
       <div className="container">
-        {image && (
-          <img
-            className="footer__logo"
-            src={image.sourceUrl}
-            alt={image.altText}
-          />
-        )}
+        <StaticImage
+          className="footer__logo"
+          src="../assets/images/logo.svg"
+          alt="Strategic Technology"
+          placeholder="none"
+        />
       </div>
     </footer>
   )
