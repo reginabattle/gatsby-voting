c7f46c41e60f0fa99c8e8f078b449ed79d699028
Regina Battle <hello@reginabattle.com>
2021-12-17 20:34:08 -0500

    get new jwt token

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,13 +1,26 @@
-const headers = {
-  "Content-Type": "application/json",
-  Accept: "application/json",
-  Authorization: "Bearer " + process.env.GATSBY_JWT_TOKEN,
+const formData = new FormData()
+formData.append("username", process.env.GATSBY_JWT_USER)
+formData.append("password", process.env.GATSBY_JWT_PASSWORD)
+
+async function getToken() {
+  let response = await fetch(process.env.GATSBY_TOKEN_URL, {
+    method: "POST",
+    body: formData,
+  })
+  let data = await response.json()
+  return data
 }
 
 export async function updateCount(e, id, count) {
+  let data = await getToken()
+
   fetch(`${process.env.GATSBY_API_URL}/charities/${id}`, {
     method: "POST",
-    headers: headers,
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      Authorization: "Bearer " + data.token,
+    },
     body: JSON.stringify({
       acf: {
         count: count + 1,
@@ -17,17 +30,5 @@ export async function updateCount(e, id, count) {
     if (res.status === 200) {
       setTimeout(() => (e.target.innerText = "Thank you!"), 2000)
     }
-  })
-}
-
-export async function resetCount(id) {
-  fetch(`${process.env.GATSBY_API_URL}/charities/${id}`, {
-    method: "POST",
-    headers: headers,
-    body: JSON.stringify({
-      acf: {
-        count: 0,
-      },
-    }),
   })
 }
